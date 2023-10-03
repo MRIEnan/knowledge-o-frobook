926300eae8aec39d5fbb552a2ec70974b6a41f97
MRIEnan <enan.mainur@gmail.com>
2023-10-03 16:58:09 +0600

    [modified]: readme

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <h1 className="text-red-800 bg-blue-600 inline-block  text-5xl font-bold underline ">
+        hello
+      </h1>
+    </div>
+  );
+}
+
+export default App;
